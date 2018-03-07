fe1f42eafb0a4dec462fb1cd8bba4b78c84dd89a
SRC8775 <sanghamitra@bitcanny.com>
2018-03-07 13:07:43 +0530

    post response changes

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET users information. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/* POST users listing. */
-router.post('/', function(req, res) {
-  console.log("====", req);
-  
+/* POST users information. */
+router.post('/', function(req, res) {  
   var user_id = req.body.id;
   var token = req.body.token;
-  var geo = req.body.geo;
+  var location = req.body.location;
+  var name = req.body.name;
+  var gender = req.body.gender;
+  var age = req.body.age;
 
-  res.send(user_id + ' ' + token + ' ' + geo);
-  //res.send('user created');
+  res.send('Id: ' + user_id + ',' + '\n' 
+  + 'Token: ' + token + ',' + '\n' 
+  + 'Location: ' + location + ',' + '\n' 
+  + 'Name: ' + name + ',' + '\n'
+  + 'Gender: ' + gender + ',' + '\n'
+  + 'Age: ' + age);
 });
 
 module.exports = router;
